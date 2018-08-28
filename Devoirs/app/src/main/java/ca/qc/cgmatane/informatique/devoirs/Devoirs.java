f28677b6196278bed4ba633051d94d52e3bc95dd
Michael <1643216@cgmatane.qc.ca>
2018-08-28 13:15:22 -0400

    Projet de base vide

---
+++ Devoirs/app/src/main/java/ca/qc/cgmatane/informatique/devoirs/Devoirs.java
@@ -0,0 +1,13 @@
+package ca.qc.cgmatane.informatique.devoirs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Devoirs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_devoirs);
+    }
+}
