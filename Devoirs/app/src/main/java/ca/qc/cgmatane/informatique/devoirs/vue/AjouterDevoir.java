c40e6930ceb6add42f0dbc13abe815d5046ae3c5
Michael <1643216@cgmatane.qc.ca>
2018-09-11 11:58:07 -0400

    Recuperation du choix de la date pour lalarme

--- Devoirs/app/src/main/java/ca/qc/cgmatane/informatique/devoirs/vue/AjouterDevoir.java
+++ Devoirs/app/src/main/java/ca/qc/cgmatane/informatique/devoirs/vue/AjouterDevoir.java
@@ -1,20 +1,22 @@
 package ca.qc.cgmatane.informatique.devoirs.vue;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 import ca.qc.cgmatane.informatique.devoirs.Devoirs;
 import ca.qc.cgmatane.informatique.devoirs.R;
 import ca.qc.cgmatane.informatique.devoirs.accesseur.DevoirsDAO;
 import ca.qc.cgmatane.informatique.devoirs.modele.Devoir;
 import org.w3c.dom.Text;
 
-public class AjouterDevoir extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AjouterDevoir extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     protected DevoirsDAO accesseurDevoirs;
     protected Devoir devoir;
 
@@ -25,6 +27,18 @@ public class AjouterDevoir extends AppCompatActivity {
 
     protected Button actionAjouterDevoir,
                         actionAjouterAlarme;
+
+    protected Calendar calendrier;
+
+    protected DatePickerDialog dialogueChoixDate;
+
+    protected int annee,
+                    mois,
+                    jour;
+
+    protected int anneeAlarme,
+                    moisAlarme,
+                    jourAlarme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +65,20 @@ public class AjouterDevoir extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("Hello", "HEY");
+                afficherDialogueChoixDate();
             }
         });
+    }
+
+    private void afficherDialogueChoixDate() {
+        calendrier = Calendar.getInstance();
+
+        annee = calendrier.get(Calendar.YEAR);
+        mois = calendrier.get(Calendar.MONTH);
+        jour = calendrier.get(Calendar.DAY_OF_MONTH);
+
+        dialogueChoixDate = new DatePickerDialog(AjouterDevoir.this, AjouterDevoir.this, annee, mois, jour);
+        dialogueChoixDate.show();
     }
 
     private void ajouterDevoir(){
@@ -64,5 +90,19 @@ public class AjouterDevoir extends AppCompatActivity {
 
     public void naviguerRetourALaBibliotheque(){
         this.finish();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        this.annee = year;
+        this.mois = month;
+        this.jour = dayOfMonth;
+
+        Log.d("HELLO", "" + annee + " " + mois + " " + jour);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
     }
 }
